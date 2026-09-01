4c16abd1ec7bd8dc129bbc5cba16b350e1064238
Naman Sharma <namansharma567891@gmail.com>
2026-09-01 07:56:26 +0000

    Restored behavior of health foss and included config based cd content access

--- cdcontent/forms.py
+++ cdcontent/forms.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 
 # Spoken Tutorial Stuff
 from creation.models import *
+from spoken import config as spoken_config
 import json
 def jsonify(data):
     return json.loads(data.replace("u'", "'").replace("'", '"'))
@@ -31,17 +32,31 @@ class CDContentForm(forms.Form):
     )
 
     def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
         super(CDContentForm, self).__init__(*args, **kwargs)
         #self.fields['language'].choices = ['nothing']
-        foss_list = [('', 'Select FOSS Category')] + list(
-            TutorialResource.objects.filter(
+        allowed_roles = getattr(spoken_config, 'CD_CONTENT_ALLOWED_ROLES', [])
+        bypass_download = False
+        if user and user.is_authenticated() and user.pk:
+            if user.is_superuser or user.groups.filter(name__in=allowed_roles).exists():
+                bypass_download = True
+
+        if bypass_download:
+            healthfosslist = list(FossCategory.objects.filter(show_on_homepage=0, foss__contains='Health').values_list('id', 'foss'))
+            foss_list = list(TutorialResource.objects.filter(
                 Q(status=1) | Q(status=2),
+                tutorial_detail__foss__show_on_homepage=1
+            ).values_list('tutorial_detail__foss_id', 'tutorial_detail__foss__foss').order_by('tutorial_detail__foss__foss').distinct()) + healthfosslist
+        else:
+            healthfosslist = list(FossCategory.objects.filter(show_on_homepage=0, foss__contains='Health', download=True).values_list('id', 'foss'))
+            foss_list = list(TutorialResource.objects.filter(
+                Q(status=1) | Q(status=2),
+                tutorial_detail__foss__show_on_homepage=1,
                 tutorial_detail__foss__download=True
-            ).values_list('tutorial_detail__foss_id', 'tutorial_detail__foss__foss')
-            .order_by('tutorial_detail__foss__foss')
-            .distinct()
-        )
-        self.fields['foss_category'].choices = foss_list
+            ).values_list('tutorial_detail__foss_id', 'tutorial_detail__foss__foss').order_by('tutorial_detail__foss__foss').distinct()) + healthfosslist
+
+        foss_choices = [('', 'Select FOSS Category')] + foss_list
+        self.fields['foss_category'].choices = foss_choices
         if args:
             print("args out ",args)
             if ('foss_category' in args[0]) and ('level' in args[0]):
